533d836c3a3b0b3716950d0380bc9184ed6cc522
Glitch (ududi-test) <none>
2020-05-26 07:57:32 +0000

    🐋💰five-hours-complete-desktop-view

--- dest/main.js
+++ dest/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
   
   
-  $('.card-state-closed .btn-open').click(function(){   
-    $('.card-state-closed').hide()
-    $('.card-state-open').show()
+  const btnOpen = document.querySelectorAll('.btn-open')[0]
+  const btnClose = document.querySelectorAll('.btn-close')[0]
+  const openState = document.querySelectorAll('.card-state-open')[0]
+  const closedState = document.querySelectorAll('.card-state-closed')[0]
+  
+  const clearActiveClasses = function(){
+      openState.classList.remove('active')
+      openState.classList.remove('wipe-up')
+      openState.removeEventListener("webkitAnimationEnd", clearActiveClasses)
+      closedState.style.opacity = 1
+    }
+   
+  btnOpen.addEventListener('click',function(){
+    openState.classList.add('active')
+    closedState.style.opacity = 0
   })
-  
-  
-  $('.card-state-open .btn-close').click(function(){
-    $('.card-state-open').hide()
-    $('.card-state-closed').show()
+
+  btnClose.addEventListener('click',function(){
+    openState.classList.add('wipe-up')
+    openState.addEventListener("webkitAnimationEnd", clearActiveClasses);
   })
-  
   
 })
