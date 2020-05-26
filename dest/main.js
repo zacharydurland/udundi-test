17530c39a0f8d381bf6da5d99d8a0424e6d93333
Glitch (udundi-test-b) <none>
2020-05-26 19:13:30 +0000

    Post Submission Update - mobile improvements - broken close button

--- dest/main.js
+++ dest/main.js
@@ -1,26 +1,36 @@
-$(document).ready(function(){
-  
-  
-  const btnOpen = document.querySelectorAll('.btn-open')[0]
-  const btnClose = document.querySelectorAll('.btn-close')[0]
-  const openState = document.querySelectorAll('.card-state-open')[0]
-  const closedState = document.querySelectorAll('.card-state-closed')[0]
-  
-  const clearActiveClasses = function(){
-      openState.classList.remove('active')
-      openState.classList.remove('wipe-up')
-      openState.removeEventListener("webkitAnimationEnd", clearActiveClasses)
-      closedState.style.opacity = 1
-    }
-   
-  btnOpen.addEventListener('click',function(){
-    openState.classList.add('active')
-    closedState.style.opacity = 0
-  })
+$(document).ready(function() {
+  const btnOpen = document.querySelectorAll(".btn-open")[0];
+  const btnClose = document.querySelectorAll(".btn-close")[0];
+  const openState = document.querySelectorAll(".card-state-open")[0];
+  const closedState = document.querySelectorAll(".card-state-closed")[0];
 
-  btnClose.addEventListener('click',function(){
-    openState.classList.add('wipe-up')
-    openState.addEventListener("webkitAnimationEnd", clearActiveClasses);
-  })
   
-})
+  // animation finish listeners added for each state for a better animation toggle
+  const transitionFromCardOpen = function() {
+    openState.classList.remove("active", "wipe-up");
+    closedState.classList.add("active");
+    openState.removeEventListener("webkitAnimationEnd", transitionFromCardOpen);
+  };
+
+  const transitionFromCardClosed = function() {
+    openState.classList.add("active");
+    closedState.removeEventListener(
+      "webkitAnimationEnd",
+      transitionFromCardClosed
+    );
+  };
+
+  btnOpen.addEventListener("click", function() {
+    closedState.classList.add("fade-out-children");
+    closedState.classList.remove("active");
+    closedState.addEventListener(
+      "webkitAnimationEnd",
+      transitionFromCardClosed
+    );
+  });
+
+  btnClose.addEventListener("click", function() {
+    openState.classList.add("wipe-up");
+    openState.addEventListener("webkitAnimationEnd", transitionFromCardOpen);
+  });
+});
